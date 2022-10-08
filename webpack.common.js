4499787537fbbb41e0f5431746bce5f654c5016c
Matt <97047080+mattxmade@users.noreply.github.com>
2022-10-08 13:17:10 +0100

    Create webpack common config; initial commit

---
+++ webpack.common.js
@@ -0,0 +1,66 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const { ESBuildMinifyPlugin } = require("esbuild-loader");
+
+module.exports = {
+  entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      minify: true,
+      title: "Shopping Cart",
+      template: "./src/index.html",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "./public", to: path.resolve(__dirname, "dist") }],
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: (pathData) => {
+      const filepath = path
+        .dirname(pathData.filename)
+        .split("/")
+        .slice(1)
+        .join("/");
+      return `${filepath}/[name][ext]`;
+    },
+    clean: true,
+  },
+  optimization: {
+    minimizer: [
+      new ESBuildMinifyPlugin({
+        target: "es2015",
+      }),
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "esbuild-loader",
+        options: {
+          loader: "jsx",
+          target: "es2015",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|webp|ico|gltf|glb|txt)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
+
+// [1] dist structure === src strucutre https://stackoverflow.com/a/68902490
