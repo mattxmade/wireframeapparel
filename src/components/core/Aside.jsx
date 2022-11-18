6c9f82c0c7299a58acb00d3dfdd832cdc9904964
Matt <97047080+mattxmade@users.noreply.github.com>
2022-11-18 00:17:49 +0000

    Rework filter handling; handle localStorage; add icon

--- src/components/core/Aside.jsx
+++ src/components/core/Aside.jsx
@@ -1,59 +1,157 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import PropTypes from "prop-types";
 
-const CategoryItem = ({ title, updateProductType }) => (
-  <li
-    onClick={(e) => {
-      updateProductType(e.target.textContent.toLowerCase());
+import AwesomeSvg from "../svg-icons/Awesome.module";
+import productCategories from "../../data/product.categories";
+import LocalStorage from "../../data/LocalStorage.module";
 
-      e.target.parentNode.childNodes.forEach((child) =>
-        child.classList.remove("li--active")
-      );
-      e.target.classList.add("li--active");
-    }}
-  >
-    {title}
-  </li>
-);
+const ProductFilters = ({
+  title,
+  active,
+  subcategories,
+  updateProductType,
+  handleActiveElement,
+  handleProductFilter,
+}) => {
+  const listRef = useRef();
+  const filters = subcategories;
+  const categoryTitle = title.toLowerCase();
 
-const Aside = ({ category, updateProductType }) => {
+  const [activeBrand, setActiveBrand] = useState(
+    LocalStorage.get("brand") ?? "All"
+  );
+
   useEffect(() => {
-    const asideListItems = document
-      .querySelector(".aside--catalog")
-      .getElementsByTagName("LI");
+    if (listRef.current.childNodes.length) {
+      listRef.current.childNodes.forEach((item) => {
+        const styling = "subcategory--active";
 
-    for (const item of asideListItems) {
-      item.textContent.toLowerCase() === category
-        ? item.classList.add("li--active")
-        : item.classList.remove("li--active");
+        item.textContent.includes(activeBrand)
+          ? item.classList.add(styling)
+          : item.classList.remove(styling);
+      });
     }
-  }, []);
+  });
+
+  const CloseIcon = AwesomeSvg.CircleXMark;
+
+  return (
+    <ul ref={listRef} className="category">
+      <li className="category-stem" />
+      <li
+        className="category-title"
+        onClick={(e) => {
+          if (categoryTitle !== active) {
+            setActiveBrand("All");
+
+            const categorySelect = e.target.textContent.toLowerCase();
+
+            if (e.target.textContent === "Reset") {
+              setTimeout(() => updateProductType("All"), 200);
+              handleActiveElement(categorySelect);
+              return;
+            }
+
+            setTimeout(() => updateProductType(categorySelect), 200);
+            handleActiveElement(e.target.textContent.toLowerCase());
+            handleProductFilter(categoryTitle, "All");
+          }
+        }}
+      >
+        {title}
+      </li>
+      {categoryTitle === active &&
+        filters &&
+        [...new Array(Object.keys(filters).length)].map((value, index) => (
+          <li
+            key={index}
+            className="subcategory"
+            onClick={(e) => {
+              setActiveBrand(e.target.textContent);
+              LocalStorage.set("brand", e.target.textContent);
+
+              handleProductFilter(categoryTitle, e.target.textContent);
+            }}
+          >
+            <div className="subcategory-branch" />
+            <span>{Object.values(filters)[index]}</span>
+          </li>
+        ))}
+    </ul>
+  );
+};
+
+ProductFilters.propTypes = {
+  title: PropTypes.string,
+  active: PropTypes.string,
+  subcategories: PropTypes.object,
+  updateProductType: PropTypes.func,
+  handleActiveElement: PropTypes.func,
+  handleProductFilter: PropTypes.func,
+};
+
+const Aside = ({ view, category, updateProductType, handleProductFilter }) => {
+  const categoryFromStorage =
+    view === "browse"
+      ? LocalStorage.get("category")?.browse
+      : LocalStorage.get("category")?.search;
+
+  const [activeElement, setActiveElement] = useState(
+    categoryFromStorage ?? category
+  );
+
+  const categoryItems = Object.entries(productCategories(view));
+
+  const handleActiveElement = (string) => {
+    if (string === activeElement) return;
+    return setActiveElement(string);
+  };
+
+  useEffect(() => {
+    const categoryTitles = document.getElementsByClassName("category-title");
+
+    for (const element of categoryTitles) {
+      element.classList.remove("category-title--active");
+      element.parentNode.classList.remove("category--active");
+
+      element.parentNode.style.height = "5rem";
+      const categoryStem = element.parentNode.childNodes[0];
+      categoryStem.style.height = "0";
+
+      if (element.textContent.toLowerCase() === activeElement) {
+        element.classList.add("category-title--active");
+        element.parentNode.classList.add("category--active");
+
+        const parentNodeHeight = (element.parentNode.children.length - 1) * 4;
+        element.parentNode.style.height = `${parentNodeHeight}rem`;
+        categoryStem.style.height = `calc(${parentNodeHeight}rem - 11.5%)`;
+      }
+    }
+  }, [activeElement]);
 
   return (
     <aside className="aside--catalog">
       <h2>// Filters</h2>
+      <CloseIcon className="close-aside" />
       <div className="aside--catalog__div">
         <h3>Category</h3>
         <ul>
-          <CategoryItem
-            title="T-shirts"
-            updateProductType={updateProductType}
-          />
+          {categoryItems.map((category, index) => {
+            const [categoryTitle, subcategories] = category;
 
-          <CategoryItem title="Hats" updateProductType={updateProductType} />
-
-          <CategoryItem
-            title="Accessories"
-            updateProductType={updateProductType}
-          />
-
-          <CategoryItem
-            title="Skateboards"
-            updateProductType={updateProductType}
-          />
-
-          <CategoryItem title="Customs" updateProductType={updateProductType} />
-
-          <CategoryItem title="Other" updateProductType={updateProductType} />
+            return (
+              <li key={index}>
+                <ProductFilters
+                  title={categoryTitle}
+                  active={activeElement}
+                  subcategories={subcategories}
+                  updateProductType={updateProductType}
+                  handleActiveElement={handleActiveElement}
+                  handleProductFilter={handleProductFilter}
+                />
+              </li>
+            );
+          })}
         </ul>
       </div>
     </aside>
@@ -61,3 +159,10 @@ const Aside = ({ category, updateProductType }) => {
 };
 
 export default Aside;
+
+Aside.propTypes = {
+  view: PropTypes.string,
+  category: PropTypes.string,
+  updateProductType: PropTypes.func,
+  handleProductFilter: PropTypes.func,
+};
