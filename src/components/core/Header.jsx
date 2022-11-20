5c512a8fac08ed3be3492bb568d814709a90c3f5
Matt <97047080+mattxmade@users.noreply.github.com>
2022-11-20 15:45:06 +0000

    Add conditional element rendering based on viewport size

--- src/components/core/Header.jsx
+++ src/components/core/Header.jsx
@@ -1,23 +1,89 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom";
+
+import "../../styles/Header.css";
+import logo from "../../assets/brand/wireframe.png";
+import AwesomeSvg from "../svg-icons/Awesome.module";
 
 const Header = (props) => {
+  const navigate = useNavigate();
   const [SearchBar, Nav, BasketWidget] = Array.from(props.children);
+  const MenuIcon = AwesomeSvg.BarsIcon;
+
+  const handleDesktopHeader = (e) => (window.innerWidth > 1000 ? true : false);
+  const handleSmallVPHeader = (e) => (window.innerWidth <= 1000 ? true : false);
+  const handleMobileHeader = (e) => (window.innerWidth <= 570 ? true : false);
+
+  const [renderDesktopHeader, setDesktopHeader] = useState(handleDesktopHeader);
+  const [renderSmallVPHeader, setSmallVPHeader] = useState(handleSmallVPHeader);
+  const [renderMobileHeader, setMobileHeader] = useState(handleMobileHeader);
+
+  const handleViewportSize = (e) => {
+    setDesktopHeader(handleDesktopHeader());
+    setSmallVPHeader(handleSmallVPHeader());
+    setMobileHeader(handleMobileHeader());
+  };
+
+  // TODO | refactor/recompose aS three elements based on viewport size
+
+  useEffect(() => {
+    window.addEventListener("resize", handleViewportSize);
+    return () => window.removeEventListener("resize", handleViewportSize);
+  }, []);
+
+  useEffect(() => {}, [renderDesktopHeader, renderSmallVPHeader]);
 
   return (
     <header>
-      <ul>
-        <li className="header__item--branding">
-          <h1>
-            Wireframe <span>Apparel</span>
+      <ul className="outline">
+        {renderSmallVPHeader && (
+          <li
+            className="header__item header__item--cart"
+            style={{
+              cursor: "pointer",
+            }}
+          >
+            {
+              <i
+                style={{
+                  width: "3rem",
+                  height: "auto",
+                  fill: "white",
+                  padding: "0.5rem",
+                  background:
+                    "linear-gradient(45deg, rgb(122, 122, 122), rgb(110, 109, 109), rgb(122, 122, 122))",
+                }}
+              >
+                <MenuIcon />
+              </i>
+            }
+          </li>
+        )}
+
+        <li
+          className="header__item header__item--branding outline "
+          onClick={() => navigate("/")}
+        >
+          <h1 className="outline">
+            Wireframe <span className="outline">Apparel</span>
           </h1>
-          <img src="" alt="wireframe apparel logo" />
+          <img className="outline" src={logo} alt="wireframe apparel logo" />
         </li>
-        <li className="header__item--search">{SearchBar}</li>
-        <li className="header__item--nav">
-          {Nav}
-          {BasketWidget}
-        </li>
+
+        {!renderMobileHeader && (
+          <li className="header__item header__item--search">{SearchBar}</li>
+        )}
+
+        {renderDesktopHeader && (
+          <li className="header__item--nav outline">{Nav}</li>
+        )}
+
+        <li className="header__item header__item--cart">{BasketWidget}</li>
+
+        {renderMobileHeader && (
+          <li className="header__item header__item--search">{SearchBar}</li>
+        )}
 
         {props.children.map((element, index) => {
           if (index >= 3) {
