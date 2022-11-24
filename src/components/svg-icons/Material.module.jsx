e140d53a7875f5c3335dabae1e65f7284ed6d790
Matt <97047080+mattxmade@users.noreply.github.com>
2022-11-24 17:12:16 +0000

    Update svg-icon modules

--- src/components/svg-icons/Material.module.jsx
+++ src/components/svg-icons/Material.module.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const MaterialSvg = (() => {
-  const BasketIcon = (props) => (
+  const CartIcon = (props) => (
     <svg
       className={props && props.className ? props.className : ""}
       xmlns="http://www.w3.org/2000/svg"
@@ -22,7 +22,7 @@ const MaterialSvg = (() => {
     </svg>
   );
 
-  return { BasketIcon };
+  return { CartIcon };
 })();
 
 export default MaterialSvg;
