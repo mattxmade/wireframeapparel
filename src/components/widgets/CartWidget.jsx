6809ddea68965cf18cba3129d28d2f6d78b3f038
Matt <97047080+mattxmade@users.noreply.github.com>
2022-11-25 16:46:48 +0000

    Add onClick navigation handling, compose elements, render component

---
+++ src/components/widgets/CartWidget.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import MaterialSvg from "../svg-icons/Material.module";
+
+const CartWidget = ({ currentOrderTotal }) => {
+  const { CartIcon } = MaterialSvg;
+  const navigate = useNavigate();
+
+  return (
+    <div className="cart-widget-cont" onClick={() => navigate("/checkout")}>
+      <p className="cart-widget__counter">{currentOrderTotal}</p>
+      <CartIcon type="sharp" className="cart-widget__icon icon" />
+    </div>
+  );
+};
+
+export default CartWidget;
