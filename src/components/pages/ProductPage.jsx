205e0cc8254945f0ff560802f69039a94b24f044
Matt <97047080+mattxmade@users.noreply.github.com>
2022-11-25 21:31:26 +0000

    Handle product attributes/selection, add validation, compose elements

---
+++ src/components/pages/ProductPage.jsx
@@ -0,0 +1,211 @@
+import React, { Fragment, useEffect, useRef, useState } from "react";
+import { useLocation } from "react-router-dom";
+
+import colorDisplayName from "../../utility/colorDisplayName";
+import ProductViewer from "../r3f/ProductViewer";
+import "../../styles/Product.css";
+
+const ProductPage = (props) => {
+  const location = useLocation();
+  const { product, handleProductToCart } = props;
+
+  const [quantity, setQuantity] = useState(1);
+  const [itemColor, setItemColor] = useState(product.color.initial);
+
+  const sizeHeadingRef = useRef();
+  const [sizeSelect, setSizeSelect] = useState(
+    product.sizing.length > 1 ? "select from below" : product.sizing.size
+  );
+
+  const handleQuantity = (action) => {
+    action === "+"
+      ? setQuantity((prevQuantity) => prevQuantity + 1)
+      : setQuantity((prevQuantity) => {
+          return prevQuantity === 1 ? prevQuantity : prevQuantity - 1;
+        });
+  };
+
+  const handleItemColor = (color) => {
+    if (color === itemColor) return;
+
+    const colorChoice = color === "#202020" ? "Black" : color;
+    product.color.choice = colorChoice;
+    setItemColor(color);
+  };
+
+  const handleSizeSelect = (size) => {
+    if (size === sizeSelect) return;
+
+    //product.sizing.size = size;
+    setSizeSelect(size);
+  };
+
+  const handleNoSizeSelected = () => {
+    sizeHeadingRef.current.style.color = "red";
+    setSizeSelect("Please select a size!");
+  };
+
+  useEffect(() => {
+    return () => {
+      console.log("unmounted product page");
+      props.handleLastPath(location.pathname);
+    };
+  }, []);
+
+  return (
+    <Fragment>
+      <main className="product-page">
+        {/* <ul className="product-images">
+          <li style={{ backgroundColor: product.color.initial }}>
+            <img src={product.image.design} />
+          </li>
+          <li>
+            <img src={product.image.src} />
+          </li>
+          <li>3D view</li>
+        </ul> */}
+
+        <ProductViewer product={product} itemColor={itemColor} />
+
+        <div className="product-details">
+          <h2 className="product-name">
+            {product.name} {product.type.kind}
+            <p>{` by ${product.brand}`}</p>
+          </h2>
+
+          <p
+            style={{
+              padding: "0.5rem",
+              borderRadius: "0.5rem",
+              backgroundColor: "#efdddd",
+            }}
+            className="product-price"
+          >
+            {"£" + product.price}
+          </p>
+
+          {/* <p>{product.type.kind}</p> */}
+
+          <div className="product-color">
+            <h3 style={{ marginBottom: "0.5rem" }}>
+              // color: {colorDisplayName(itemColor)}
+            </h3>
+            <ul
+              style={{
+                width: "fit-content",
+                height: "max-content",
+                display: "flex",
+                flexWrap: "wrap",
+                padding: "0.5rem",
+                borderRadius: "0.5rem",
+                backgroundColor: "#efdddd",
+              }}
+            >
+              {product.color.options.map((col, index) => (
+                <li
+                  key={index}
+                  onClick={() => handleItemColor(col)}
+                  style={{
+                    cursor: "pointer",
+
+                    margin: "0.5rem",
+                    backgroundColor: col,
+                    boxShadow: "0 0 0.2rem 0.1rem black",
+                  }}
+                />
+              ))}
+            </ul>
+          </div>
+
+          <div className="product-sizing">
+            <h3 ref={sizeHeadingRef} style={{ marginBottom: "0.5rem" }}>
+              // size: {sizeSelect}
+            </h3>
+            <ul
+              className="product-sizing__list"
+              style={{
+                width: "fit-content",
+                height: "max-content",
+                display: "flex",
+                justifyContent: "space-evenly",
+                flexWrap: "wrap",
+                padding: "0.5rem",
+                borderRadius: "0.5rem",
+                backgroundColor: "#efdddd",
+              }}
+            >
+              {product.sizing.chart.map((size, index) => (
+                <li
+                  className="size"
+                  key={index}
+                  onClick={(e) => {
+                    sizeHeadingRef.current.style.color = "black";
+
+                    e.target.parentNode.childNodes.forEach((node) => {
+                      node.style.color = "black";
+                      node.style.backgroundColor = "aliceblue";
+                    });
+
+                    e.target.style.color = "white";
+                    e.target.style.backgroundColor = "black";
+                    handleSizeSelect(size);
+                  }}
+                  style={{
+                    cursor: "pointer",
+                    width: "4rem",
+                    height: "auto",
+                    margin: "0.5rem",
+                    textAlign: "center",
+                    backgroundColor: "aliceblue",
+                    boxShadow: "0 0 0.2rem 0.1rem black",
+                  }}
+                >
+                  {size}
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="product-options">
+            <div className="quantity-options">
+              <button onClick={(e) => handleQuantity(e.target.textContent)}>
+                <div className="box-mask"></div>-
+              </button>
+              <p>{quantity}</p>
+              <button onClick={(e) => handleQuantity(e.target.textContent)}>
+                +
+              </button>
+            </div>
+            <button
+              className="add-to-cart"
+              onClick={() => {
+                if (sizeSelect === "unset") return handleNoSizeSelected();
+
+                const newCartItem = {
+                  id: product.id,
+                  size: sizeSelect,
+                  name: product.name,
+                  brand: product.brand,
+                  image: product.image,
+                  price: product.price,
+                  type: product.type.kind,
+                  color: product.color.choice,
+                  variant:
+                    product.id.toString() +
+                    sizeSelect.toString() +
+                    product.color.choice.toString(),
+                };
+
+                handleProductToCart(newCartItem, quantity);
+              }}
+            >
+              Add to Cart
+            </button>
+          </div>
+        </div>
+      </main>
+    </Fragment>
+  );
+};
+
+export default ProductPage;
