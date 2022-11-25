a7443af03255045fd7345eb1831d8cca5a52548b
Matt <97047080+mattxmade@users.noreply.github.com>
2022-11-25 17:00:27 +0000

    Setup root, nest elements, implement HashRouter

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
+import "./index.css";
 import App from "./components/App";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
