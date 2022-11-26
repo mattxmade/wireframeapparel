737b9911bcb33f68f5ce6a4dbe632f5decff66a2
Matt <97047080+mattxmade@users.noreply.github.com>
2022-11-26 01:29:42 +0000

    Handle product image imports, return cache

---
+++ src/assets/AssetsFromDirectory.js
@@ -0,0 +1,40 @@
+// https://stackoverflow.com/a/67326513
+
+const AssetsFromDirectory = (directory, imageType) => {
+  const cache = {};
+
+  // handle imports
+  const getAssets = (r) => {
+    r.keys().forEach((key) => (cache[key] = r(key)));
+  };
+
+  switch (directory) {
+    case "designs":
+      getAssets(require.context("./designs", false, /\.(webp)$/));
+      break;
+
+    case "tshirts":
+      getAssets(require.context("./catalog/tshirts", false, /\.(webp)$/));
+      break;
+
+    case "footwear":
+      getAssets(require.context("./catalog/footwear", false, /\.(webp)$/));
+      break;
+
+    case "skateboards":
+      getAssets(require.context("./catalog/skateboards", false, /\.(webp)$/));
+      break;
+  }
+
+  const cacheArray = [];
+
+  Object.entries(cache).map((module) => {
+    const uri = module[1];
+    const name = module[0].replace("./", "");
+    cacheArray.push({ uri, name });
+  });
+
+  return cacheArray;
+};
+
+export default AssetsFromDirectory;
