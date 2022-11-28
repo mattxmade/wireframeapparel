591b735e8bb6784577dd26ed50ef957f97a7c850
Matt <97047080+mattxmade@users.noreply.github.com>
2022-11-28 19:13:40 +0000

    Set css scss loader, add MiniCss + sass-loader to module rules

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,32 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
   devtool: "source-map",
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "styles/[name].css",
+      // chunkFilename: "styles/[name].css",
+    }),
+  ],
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
 });
