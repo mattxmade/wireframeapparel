049e21c7d58b60d1899eabe294072bb87bd5f43b
Matt <97047080+mattxmade@users.noreply.github.com>
2022-11-29 01:06:49 +0000

    Create ResultCard component: handle props, compose elements, set proptypes

---
+++ src/components/pages/ResultCard.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+import "./ResultCard.style.scss";
+
+const enhanceImage = (e) => {
+  e.target.parentNode.nextSibling.style.width = "256px";
+};
+
+const resetImage = (e) => {
+  e.target.parentNode.nextSibling.style.width = "100%";
+};
+
+const ResultCard = ({ item, type, handleProductSelection }) => {
+  const ref = useRef();
+  const maskRef = useRef();
+  const productRef = useRef();
+
+  useEffect(() => {
+    // maskRef.current.addEventListener("mouseover", enhanceImage);
+    // maskRef.current.addEventListener("mouseleave", resetImage);
+    // cleanup remove ??
+
+    maskRef.current.classList.add("reveal-item");
+    productRef.current.classList.add("clip-container-path");
+  }, []);
+
+  return (
+    <div ref={productRef} className="product-result">
+      <Link ref={ref} to={`/shop/${item.id}`}>
+        <div
+          ref={maskRef}
+          className="product-mask"
+          onClick={() => {
+            const selectedItem = item;
+            handleProductSelection(selectedItem);
+          }}
+        />
+      </Link>
+
+      <img src={item.image.src} alt={item.image.alt} />
+
+      <div className="product-result__content">
+        <h4 className="product-name">{`${item.name} ${item.type.kind}`}</h4>
+        <p className="product-price">{"£" + item.price}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ResultCard;
+
+ResultCard.propTypes = {
+  item: PropTypes.object,
+  type: PropTypes.string,
+  handleProductSelection: PropTypes.func,
+};
