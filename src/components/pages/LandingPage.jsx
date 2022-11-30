bbe469f2a098273be8c32f03c9e57666b81c0d73
Matt <97047080+mattxmade@users.noreply.github.com>
2022-11-30 17:03:09 +0000

    Add elements, implement animations

--- src/components/pages/LandingPage.jsx
+++ src/components/pages/LandingPage.jsx
@@ -1,5 +1,5 @@
-import React, { Fragment, useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import React, { Fragment, useEffect, useRef, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 import Carousel from "../widgets/Carousel";
 import GridPlaneSvg from "../svg-images/GridPlaneSvg";
@@ -8,8 +8,37 @@ import AssetsFromDirectory from "../../assets/AssetsFromDirectory";
 
 import "./LandingPage.style.scss";
 
+const animations = {
+  translatePositionY: {
+    animation: [
+      { transform: "translate(0, -30vh)" },
+      { transform: "translate(0,0)" },
+    ],
+  },
+  colorToTransparent: {
+    animation: [
+      { color: "black", backgroundColor: "aliceblue" },
+      { color: "transparent", backgroundColor: "transparent" },
+    ],
+  },
+  transparentToColor: {
+    animation: [
+      { color: "transparent", backgroundColor: "transparent" },
+      { color: "black", backgroundColor: "aliceblue" },
+    ],
+  },
+};
+
+const animTiming = (delay, duration, iterations, fill) => {
+  return { fill, delay, duration, iterations };
+};
+
+const getCarouselHeight = () =>
+  getComputedStyle(document.querySelector(".carousel-container")).height;
+
 const LandingPage = (props) => {
   const location = useLocation();
+  const navigate = useNavigate();
   const visited = LocalStorage.get("visited") ?? false;
 
   const tshirts = AssetsFromDirectory("tshirts");
@@ -25,6 +54,21 @@ const LandingPage = (props) => {
     })
   );
 
+  const refHeading = useRef();
+
+  const [actionText, setActionText] = useState(
+    !visited ? "// WELCOME TO WIREFRAME APPAREL" : "// SHOP APPAREL"
+  );
+
+  const animationProps_h2 = !visited ? "apply-anims__h2-call-to-action" : "";
+
+  const [cursor, setCursor] = useState("auto");
+  const [styleProps, setStyleProps] = useState({});
+
+  const handleViewportChange = (e) => {
+    setTimeout(() => setStyleProps({ height: getCarouselHeight() }), 1000);
+  };
+
   useEffect(() => {
     const header = document.querySelector("header");
 
@@ -34,14 +78,32 @@ const LandingPage = (props) => {
     const grid = document.querySelector("#svg-line-grid");
     const gridSky = document.querySelector("#svg-rect-sky");
     const gridPln = document.querySelector("#svg-grid-plane");
+
+    const action = document.querySelector(".landing__call-to-action");
     const carousel = document.querySelector(".carousel");
 
     if (!visited) {
       setTimeout(() => {
-        return carousel.parentNode.classList.add(
-          "animate--carousel__clip-path"
-        );
-      }, 1200);
+        carousel.parentNode.classList.add("animate--carousel__clip-path");
+      }, 4000); // 2000// 1200
+
+      setTimeout(() => {
+        setActionText("// SHOP APPAREL");
+        setCursor("pointer");
+      }, 6000);
+
+      refHeading.current.animate(
+        animations.colorToTransparent.animation,
+        animTiming(4000, 2000, 1, "forwards")
+      );
+      refHeading.current.animate(
+        animations.translatePositionY.animation,
+        animTiming(6250, 0, 1, "forwards")
+      );
+      refHeading.current.animate(
+        animations.transparentToColor.animation,
+        animTiming(7000, 600, 1, "forwards")
+      );
 
       let offset = 200;
 
@@ -52,7 +114,7 @@ const LandingPage = (props) => {
       setTimeout(() => {
         grid.classList.add("animate--grid__fill");
         gridCont.classList.add("animate--grid__rotation");
-      }, 400 + offset);
+      }, 200 + offset);
 
       setTimeout(() => {
         pageMask.classList.add("animate--mask_z-index");
@@ -68,13 +130,24 @@ const LandingPage = (props) => {
       pageMask.style.zIndex = -100;
       grid.style.fill = "aliceblue";
 
+      setCursor("pointer");
+
+      refHeading.current.animate(
+        animations.translatePositionY.animation,
+        animTiming(0, 0, 1, "forwards")
+      );
+
       carousel.parentNode.style.clipPath =
         "polygon(0 0, 100% 0, 100% 100%, 0% 100%)";
     }
 
+    handleViewportChange();
+    window.addEventListener("resize", handleViewportChange);
+
     return () => {
       header.classList.remove("landing__header");
       props.handleLastPath(location.pathname);
+      window.removeEventListener("resize", handleViewportChange);
     };
   }, []);
 
@@ -83,11 +156,18 @@ const LandingPage = (props) => {
       <div id="mask" />
 
       <main className="main--landing">
-        <ul className="landing__brand-content">
-          <li className="brand-content__item">1</li>
-          <li className="brand-content__item">2</li>
-          <li className="brand-content__item">3</li>
-        </ul>
+        <div
+          style={{ cursor: cursor }}
+          className="landing__call-to-action"
+          onClick={() => navigate("/shop")}
+        >
+          <h2
+            ref={refHeading}
+            className={`h2-call-to-action ${animationProps_h2}`}
+          >
+            {actionText}
+          </h2>
+        </div>
 
         <Carousel />
 
@@ -101,51 +181,6 @@ const LandingPage = (props) => {
             }`}
           />
         </div>
-
-        <ul
-          style={{
-            zIndex: 900,
-            position: "absolute",
-            bottom: "2.5rem",
-            display: "flex",
-            width: "100%",
-            overflow: "hidden",
-            justifyContent: "center",
-          }}
-        >
-          {imageTree &&
-            imageTree.map((image, index) => (
-              <li
-                key={index}
-                style={{
-                  gap: "0.5rem",
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <img
-                  style={{
-                    width: `calc(100vw/${imageTree.length})`,
-                  }}
-                  src={image.uri}
-                  alt={image.name}
-                />
-
-                <div
-                  className="image-shadow"
-                  style={{
-                    borderRadius: "100%",
-                    width: `calc(100vw/${imageTree.length} / 2)`,
-                    height: "1rem",
-                    opacity: 0.6,
-                    background: "darkslategrey",
-                  }}
-                />
-              </li>
-            ))}
-        </ul>
       </main>
     </Fragment>
   );
