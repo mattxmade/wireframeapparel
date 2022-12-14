5e4f49db6f2eb158ef74348fe4d9453176966548
Matt <97047080+mattxmade@users.noreply.github.com>
2022-12-14 12:06:54 +0000

    Make use of HOC handleCount function and count prop

--- src/components/widgets/CartWidget.jsx
+++ src/components/widgets/CartWidget.jsx
@@ -4,17 +4,20 @@ import { useNavigate } from "react-router-dom";
 import MaterialSvg from "../svg-icons/Material.module";
 import "./CartWidget.style.scss";
 
-const CartWidget = ({ currentOrderTotal }) => {
+const CartWidget = (props) => {
   const { CartIcon } = MaterialSvg;
   const navigate = useNavigate();
 
+  // withCounter - HOC function handler
+  props.handleCount(props.customerOrder);
+
   return (
     <button
-      aria-label={`Items in cart: ${currentOrderTotal}`}
+      aria-label={`Items in cart: ${props.count}`}
       className="cart-widget-button icon--select"
       onClick={() => navigate("/checkout")}
     >
-      <p className="cart-widget__counter">{currentOrderTotal}</p>
+      <p className="cart-widget__counter">{props.count}</p>
       <CartIcon type="sharp" className="cart-widget__icon icon" />
     </button>
   );
