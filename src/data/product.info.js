4117a4a848b651279a50e8729917eba4905b979c
Matt <97047080+mattxmade@users.noreply.github.com>
2023-07-24 17:57:50 +0100

    Create getItemData function; add product Ids + prices

---
+++ src/data/product.info.js
@@ -0,0 +1,47 @@
+export const getItemData = (category) => {
+  switch (category) {
+    case "hats":
+      return ["A0KLMZTSfNOU3rNDSACms"];
+    case "t-shirts":
+      return [
+        { id: "LMFgbsbBv8RTeDEbqKJKe", price: 37 },
+        { id: "2aEcSl_ekwMM6Y_GCfbog", price: 37 },
+        { id: "00P9dkV6fmCyErwinMYtj", price: 38.99 },
+        { id: "geRQ8GhwTTOKT4aCy18VY", price: 42 },
+        { id: "LCxJOSMdYJtEpe0M4gyZw", price: 33.99 },
+        { id: "1JGWQbGeEcBWpbBHvQEJS", price: 40 },
+        { id: "wFbU_4XvmZn5uHSW3h614", price: 35.5 },
+        { id: "1oceKGeKwCAQzteb5i1Rf", price: 45 },
+        { id: "YMiJ8V8-fvksl7M-X8uKn", price: 45 },
+        { id: "uX9nbW8w84OwHZgDA2inz", price: 40.5 },
+        { id: "U-KE4gfi4w_TL25nLAcat", price: 39.99 },
+        { id: "gLoSPcCbpg16dfLB_xKTb", price: 35 },
+        { id: "BY7Fwrspg6IFXHIOUiwrZ", price: 40 },
+        { id: "pHCBfYFkA6PrDvdcd-eE8", price: 38 },
+        { id: "qa4egQOw0WasJLyjEypRc", price: 45 },
+        { id: "DaT-r7dc5SqtrN8s4J2A5", price: 45 },
+        { id: "CnyU3y-fBeuGKSx3nVnNT", price: 39.5 },
+        { id: "unPoj_1511ojV9NiA9gEn", price: 40 },
+        { id: "rK0MxlyyfsunQxKGm22xt", price: 35 },
+        { id: "xysapOFyBKs39pbUJTbQQ", price: 45 },
+      ];
+    case "footwear":
+      return [{ id: "7xrCc79aOUwIeM5D7wMaF", price: 60 }];
+    case "skateboards":
+      return [
+        { id: "AlS6z5Lcg1ZC0f5cLoHly", price: 70 },
+        { id: "UJYCwGjF-nngIDSQtQOyS", price: 75 },
+        { id: "LhG2ft1-OtV7-h5VYOPs8", price: 74.99 },
+        { id: "EkRWRo_R7JPwMkf-kxJt1", price: 79.99 },
+        { id: "q8DYE-YXwVMAJGIJtzpWE", price: 75 },
+        { id: "kBgd7lvFf82RfpPAF4yPb", price: 70 },
+        { id: "TouMNaddorvFGAgPrVXY4", price: 75 },
+        { id: "H6GQfygOyGtEC8QycxGQx", price: 70 },
+        { id: "RAXnppvfb9aQlZ9EnYqgW", price: 75 },
+        { id: "kU3Rxt3_iOzXx4o87FNmd", price: 79.99 },
+        { id: "8ChzExZaYsNOnZ64EOZte", price: 70 },
+        { id: "vNIWNEFBXf-CzBLABL6r5", price: 75 },
+        { id: "JYIs0XYEE8fSko8uvu-Ku", price: 79.99 },
+      ];
+  }
+};
