2b1a8c5f136db3bfc73da8d4b0149bd2e55e1362
Matt <97047080+mattxmade@users.noreply.github.com>
2023-07-24 14:08:53 +0100

    Add no match page for unknown url; handle last page if refresh

--- src/components/pages/NoMatchPage.jsx
+++ src/components/pages/NoMatchPage.jsx
@@ -1,11 +1,27 @@
 import React, { useEffect } from "react";
+import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
-const NoMatchPage = () => {
+import fetchData from "../../data/fetchData";
+import ProductData from "../../data/product.data";
+
+const NoMatchPage = (props) => {
   let location = useLocation();
   const navigate = useNavigate();
+  const pathname = location.pathname.slice(-21);
 
-  useEffect(() => navigate("/shop"), []);
+  const [product, setProduct] = useState();
+
+  const getProductByID = () =>
+    fetchData(ProductData.get, { setProduct }, pathname);
+
+  useEffect(() => {
+    if (pathname.length !== 21) navigate("/");
+
+    getProductByID();
+
+    product ? props.handleProductSelection(product) : 0;
+  }, [product]);
 
   return (
     <div>
