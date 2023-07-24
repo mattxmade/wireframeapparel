41dd185374da03b9bc0736dbd8438bac3ffbec7f
Matt <97047080+mattxmade@users.noreply.github.com>
2023-07-24 16:41:52 +0100

    Create module; create checkoutPlaceholder object; initial commit

---
+++ src/data/placeholderContent.js
@@ -0,0 +1,10 @@
+export const checkoutPlaceholder = {
+  first: "Sherlock",
+  last: "Holmes",
+  email: "elementary@outlook.com",
+  telephone: "12345789",
+  address: "221B",
+  street: "Baker Street",
+  city: "London",
+  postcode: "NW1 6XE",
+};
