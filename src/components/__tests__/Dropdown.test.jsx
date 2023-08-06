cfde5f368418bfc2c1ea37cbc7e3ca5b3ba25abb
Matt <97047080+mattxmade@users.noreply.github.com>
2023-08-06 19:01:02 +0100

    Change role: listitem => button | test against first option in array; remove array map

--- src/components/__tests__/Dropdown.test.jsx
+++ src/components/__tests__/Dropdown.test.jsx
@@ -8,12 +8,10 @@ import "@testing-library/dom";
 import Dropdown from "../widgets/Dropdown";
 
 // arrange act assert
-const handleSetSort = jest.fn;
 const handleSortProductsBy = jest.fn;
 
 const props = {
-  children: ["name: a-z", "price: low to high", "price: high to low"],
-  handleSetSort,
+  options: ["name: a-z", "price: low to high", "price: high to low"],
   handleSortProductsBy,
 };
 
@@ -25,43 +23,39 @@ describe("Dropdown Component unit tests", () => {
     expect(asFragment).toMatchSnapshot();
   });
 
-  it("Renders only primary list item on mount", () => {
+  it("On mount, renders first item of options prop as primary item", () => {
     render(<Dropdown {...props} />);
-    const listItems = screen.getAllByRole("listitem");
+    const listItems = screen.getAllByRole("button");
 
     expect(listItems.length).toEqual(1);
-
-    listItems.map(
-      (listItem, index) =>
-        expect(listItem.textContent.trim()).toBe(props.children[index]) // [1]
-    );
+    expect(listItems[0].textContent.trim()).toBe(props.options[0]);
   });
 
   it("Renders expected list items on dropdown toggle", async () => {
     render(<Dropdown {...props} />);
-    const toggleDropdown = screen.getByRole("listitem");
+    const toggleDropdown = screen.getByRole("button");
 
     await user.click(toggleDropdown);
-    const listItems = await screen.findAllByRole("listitem");
+    const listItems = await screen.findAllByRole("button");
 
-    expect(listItems.length).toEqual(props.children.length + 1);
+    expect(listItems.length).toEqual(props.options.length + 1);
   });
 
   it("On click, renders dropdown option as primary list item", async () => {
     render(<Dropdown {...props} />);
-    const toggleDropdown = screen.getByRole("listitem");
+    const toggleDropdown = screen.getByRole("button");
 
     await user.click(toggleDropdown);
-    const dropdownOpen = await screen.findAllByRole("listitem");
+    const dropdownOpen = await screen.findAllByRole("button");
 
-    const option = await props.children.map((item, index) => index + 1)[
-      Math.floor(Math.random() * props.children.length)
+    const option = props.options.map((item, index) => index + 1)[
+      Math.floor(Math.random() * props.options.length)
     ];
 
     const elementFromDropdown = dropdownOpen[option];
 
     await user.click(dropdownOpen[option]);
-    const dropdownClose = await screen.findAllByRole("listitem");
+    const dropdownClose = await screen.findAllByRole("button");
 
     expect(dropdownClose.length).toEqual(1);
     expect(dropdownClose[0].textContent.trim()).toBe(
