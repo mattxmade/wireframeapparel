456e308269ffa6f3ba8a038bfaa93788d8666bd4
Matt <97047080+mattxmade@users.noreply.github.com>
2023-08-25 12:47:58 +0100

    Update props; remove useEffect

--- src/components/widgets/CartWidget.jsx
+++ src/components/widgets/CartWidget.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 
 import MaterialSvg from "../svg-icons/Material.module";
@@ -8,15 +8,13 @@ const CartWidget = (props) => {
   const { CartIcon } = MaterialSvg;
   const navigate = useNavigate();
 
-  useEffect(() => props.handleCartCount(props.customerOrder), []);
-
   return (
     <button
-      aria-label={`Items in cart: ${props.numberOfItemsInCart}`}
+      aria-label={`Items in cart: ${props.itemCount}`}
       className="cart-widget-button icon--select"
       onClick={() => navigate("/checkout")}
     >
-      <p className="cart-widget__counter">{props.numberOfItemsInCart}</p>
+      <p className="cart-widget__counter">{props.itemCount}</p>
       <CartIcon type="sharp" className="cart-widget__icon icon" />
     </button>
   );
